8dc3f4fc8a9f099d68ccc0126928b86a04798a76
kkawakami08 <k.kawakami08@gmail.com>
2022-08-05 11:50:08 -0700

    add 3 random artists each with 10 songs to choose

---
+++ javascript.js
@@ -0,0 +1,41 @@
+const artist = document.getElementsByTagName("span")[0];
+const song = document.getElementsByTagName("h2")[0];
+
+const artistArray = ["Olivia Rodrigo", "Billie Eilish", "Harry Styles"];
+
+const billieSongs = [
+  "Bellyache",
+  "Ocean Eyes",
+  "My Future",
+  "When the Party's Over",
+  "Oxytocin",
+  "Your Power",
+  "Bad Guy",
+  "Happier Than Ever",
+];
+
+const oliviaSongs = [
+  "Brutal",
+  "Hope U R Ok",
+  "1 Step Forward, 3 Steps Back",
+  "Jealousy Jealousy",
+  "Happier",
+  "Enough for You",
+  "Traitor",
+  "Driver's License",
+  "Deja vu",
+  "Good 4 U",
+];
+
+const harrySongs = [
+  "Fine Line",
+  "Sign of the Times",
+  "As It Was",
+  "Late Night Talking",
+  "Golden",
+  "Matilda",
+  "She",
+  "Music For a Sushi Restaurant",
+  "Cherry",
+  "Ever Since New York",
+];
